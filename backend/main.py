33f5abe783bfaa1dd93e64596eff8316e9ee9607
Eric Andrechek <ericandrechek@Erics-MBP.lan>
2020-08-15 04:03:04 -0400

    integrated db with flask

--- backend/main.py
+++ backend/main.py
@@ -1,46 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from db import get_all, get_id, add_incident, add_request
 
 app = Flask(__name__)
 
 @app.route("/all", methods = ['GET'])
 def all_disaster_data():
-  data = [
-    {
-      "id": "312hjhjh12",
-      "location": "JPN",
-      "color": "blue",
-      "damages": 15,
-      "highlighted": True
-    },
-    {
-      "id": "djhfg82934h",
-      "location": "Okemos, Michigan, USA",
-      "color": "orange",
-      "damages": 29,
-      "highlighted": False
-    }
-  ]
-  return jsonify(data)
+  return jsonify(get_all())
 
 @app.route("/get", methods = ["GET"])
 def specific_disaster_data():
-  data2 = [
-    {
-      "id": "312hjhjh12",
-      "location": "JPN",
-      "color": "blue",
-      "damages": 15,
-      "highlighted": True
-    },
-    {
-      "id": "djhfg82934h",
-      "location": "Okemos, Michigan, USA",
-      "color": "orange",
-      "damages": 29,
-      "highlighted": False
-    }
-  ]
-  return jsonify(data2)
+  id = request.args.get('id')
+  data = get_id(id)
+  if data == False:
+      data = {"error": "not a valid id"}
+  return jsonify(data)
 
 
 if  __name__ == '__main__':

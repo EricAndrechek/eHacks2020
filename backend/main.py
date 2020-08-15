b4275fb236c0945edc83017ece784f667fc5c5d8
Eric Andrechek <ericandrechek@Erics-MBP.lan>
2020-08-15 01:40:54 -0400

    try this Grant

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/all", methods = ['GET'])
 def all_disaster_data():
-  data = {
+  data = [
     {
       "id": "312hjhjh12",
       "location": "JPN",
@@ -19,11 +19,8 @@ def all_disaster_data():
       "damages": 29,
       "highlighted": False
     }
-  }
+  ]
   return jsonify(data)
-
-# why is there an unexpected indent what
-# can u git push i want to try this on mine yeah
 
 if  __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')

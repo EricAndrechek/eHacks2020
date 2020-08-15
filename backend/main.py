b8e71b8b6dad5fe1be79a1b0f57b137175afb56e
Eric Andrechek <ericandrechek@Erics-MBP.lan>
2020-08-15 04:11:43 -0400

    merge errors suck

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route("/all", methods = ['GET'])
 def all_disaster_data():
-  return jsonify(get_all())
+    return jsonify(get_all())
 
 @app.route("/get", methods = ["GET"])
 def specific_disaster_data():
-  id = request.args.get('id')
-  data = get_id(id)
-  if data == False:
-      data = {"error": "not a valid id"}
-  return jsonify(data)
+    id = request.args.get('id')
+    data = get_id(id)
+    if data == False:
+            data = {"error": "not a valid id"}
+    return jsonify(data)
 
 
-if  __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+if    __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

fa527b08615f06c1fc5a7eeb766bde2d2c5b7c7f
Grant James <69410061+GrantJames12@users.noreply.github.com>
2020-08-15 04:06:05 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -41,6 +41,14 @@ def specific_disaster_data():
     }
   ]
   return jsonify(data2)
+
+@app.route("/add", methods = ["POST"])
+def add_disaster_data():
+    default_name = "0"
+    location = input("Enter Location:")
+    damages = input("Enter Damage Costs")
+    data3 = request.form.get["POST", default_name]
+    return data3
 
 
 if  __name__ == '__main__':
